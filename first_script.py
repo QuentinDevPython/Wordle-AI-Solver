f331987b57f6581de5b1a8d1218e1fe024ee4dff
QuentinDevPython <quentin.barthelemy.01@gmail.com>
2023-01-09 23:34:49 +0100

    Implementing Random IA display

--- first_script.py
+++ first_script.py
@@ -1,6 +1,8 @@
 import random
 import pygame
 
+import time
+
 from random_ia import random_IA
 
 def load_english_words(file_name):
@@ -403,11 +405,171 @@ while running:
             
             if pos[0] > WINDOW_WIDTH-260 and pos[0] < WINDOW_WIDTH-240:
                 if pos[1] > 140 and pos[1] < 160:
-                    checkboxes = [checkboxes[0], False, False, False]
                     checkboxes[0] = not checkboxes[0]
+                    checkboxes = [checkboxes[0], False, False, False]
+
+                    # CheckBox 1
+                    checkbox1 = pygame.Rect(WINDOW_WIDTH-260, TOP_MARGIN//2 + 90, 20, 20)
+                    if not checkboxes[0]:
+                        pygame.draw.rect(screen, GREY, checkbox1, width=2)
+                    else:
+                        pygame.draw.rect(screen, GREY, checkbox1, border_radius=3)
+
+                    ia = LETTERS_FONT_VERY_SMALL.render("IA - RANDOM -", False, GREY)
+                    surface = ia.get_rect(center = (WINDOW_WIDTH-160, TOP_MARGIN//2 + 100))
+                    screen.blit(ia, surface)
+
+                    # CheckBox 2
+                    checkbox2 = pygame.Rect(WINDOW_WIDTH-260, TOP_MARGIN//2 + 135, 20, 20)
+                    if not checkboxes[1]:
+                        pygame.draw.rect(screen, GREY, checkbox2, width=2)
+                    else:
+                        pygame.draw.rect(screen, GREY, checkbox2, border_radius=3)
+                    pygame.draw.rect(screen, GREY, checkbox2, width=2)
+
+                    ia = LETTERS_FONT_VERY_SMALL.render("IA - ALGORITHM -", False, GREY)
+                    surface = ia.get_rect(center = (WINDOW_WIDTH-148, TOP_MARGIN//2 + 147))
+                    screen.blit(ia, surface)
+
+                    # CheckBox 3
+                    checkbox3 = pygame.Rect(WINDOW_WIDTH-260, TOP_MARGIN//2 + 180, 20, 20)
+                    if not checkboxes[2]:
+                        pygame.draw.rect(screen, GREY, checkbox3, width=2)
+                    else:
+                        pygame.draw.rect(screen, GREY, checkbox3, border_radius=3)
+                    pygame.draw.rect(screen, GREY, checkbox3, width=2)
+
+                    ia = LETTERS_FONT_VERY_SMALL.render("IA - MINIMAX -", False, GREY)
+                    surface = ia.get_rect(center = (WINDOW_WIDTH-163, TOP_MARGIN//2 + 191))
+                    screen.blit(ia, surface)
+
+                    # CheckBox 3
+                    checkbox4 = pygame.Rect(WINDOW_WIDTH-260, TOP_MARGIN//2 + 225, 20, 20)
+                    if not checkboxes[3]:
+                        pygame.draw.rect(screen, GREY, checkbox4, width=2)
+                    else:
+                        pygame.draw.rect(screen, GREY, checkbox4, border_radius=3)
+                    pygame.draw.rect(screen, GREY, checkbox4, width=2)
+
+                    ia = LETTERS_FONT_VERY_SMALL.render("IA - DEEP LEARNING -", False, GREY)
+                    surface = ia.get_rect(center = (WINDOW_WIDTH-130, TOP_MARGIN//2 + 236))
+                    screen.blit(ia, surface)
 
                     # Lancer l'IA Random
-                    GUESSES = random_IA(WORD_TO_GUESS)
+                    if checkboxes[0]:
+                        CHANCES = 6
+                        letters_not_to_touch = []
+                        WIN = False
+                        DEFEAT = False
+                        words_to_keep = ENGLISH_WORDS_5_LETTERS
+                        for chance_number in range(1, CHANCES+1):
+                            GUESSES, WIN, DEFEAT, words_to_keep, letters_not_to_touch = random_IA(
+                                WORD_TO_GUESS,
+                                GUESSES,
+                                words_to_keep,
+                                letters_not_to_touch,
+                                chance_number,
+                                WIN,
+                                DEFEAT
+                            )
+                            # Draw words guesses grid
+                            y = TOP_MARGIN//2
+                            for i in range(6):
+                                x = WINDOW_WIDTH//5
+                                for j in range(5):
+
+                                    # Letter squares
+                                    letter_square = pygame.Rect(x, y, SQUARES_SIZE, SQUARES_SIZE)
+                                    pygame.draw.rect(screen, GREY, letter_square, width=2)
+
+                                    # letters / words already guessed
+                                    if i < len(GUESSES):
+                                        color = determine_color(GUESSES[i], j)
+                                        pygame.draw.rect(screen, color, letter_square, border_radius=3)
+                                        letter = LETTERS_FONT.render(GUESSES[i][j], False, (255,255,255))
+                                        surface = letter.get_rect(center = (x+SQUARES_SIZE//2, y+SQUARES_SIZE//2))
+                                        screen.blit(letter, surface)
+
+                                    # User input (next guess)
+                                    if i == len(GUESSES) and j < len(INPUT):
+                                        letter = LETTERS_FONT.render(INPUT[j], False, GREY)
+                                        surface = letter.get_rect(center = (x+SQUARES_SIZE//2, y+SQUARES_SIZE//2))
+                                        screen.blit(letter, surface)
+
+                                    x += SQUARES_SIZE + MARGIN
+
+                                y += SQUARES_SIZE + MARGIN
+                                
+                            # Draw unguessed letters
+                            y = TOP_MARGIN//2 + 6 * (SQUARES_SIZE + MARGIN)  + TOP_MARGIN//2 + 50
+
+                            LETTERS_SIZE = (WINDOW_GRID_WIDTH - 4*MARGIN - 2*LEFT_MARGIN) // 7
+                            colors = determine_color_alphabet()
+
+                            for i in range(4):
+                                x = WINDOW_WIDTH//6
+                                if i != 3:
+                                    for j in range(7):
+                                        # Letter squares
+                                        letter_square = pygame.Rect(x, y, LETTERS_SIZE, LETTERS_SIZE)
+                                        color = (255,255,255)
+                                        color_letter = GREY
+                                        for item in colors:
+                                            if (i,j) == item[0]:
+                                                color = item[1]
+                                                color_letter = (255,255,255)
+                                        pygame.draw.rect(screen, color, letter_square, border_radius=3)
+                                        pygame.draw.rect(screen, GREY, letter_square, width=2)
+                                        letter = LETTERS_FONT_SMALL.render(ALPHABET[i][j], False, color_letter)
+                                        surface = letter.get_rect(center = (x+LETTERS_SIZE//2, y+LETTERS_SIZE//2))
+                                        screen.blit(letter, surface)
+
+                                        x += LETTERS_SIZE + 2*MARGIN
+
+                                else:
+                                    for j in range(5):
+                                        # Letter squares
+                                        letter_square = pygame.Rect(x + (LETTERS_SIZE + 2*MARGIN), y, LETTERS_SIZE, LETTERS_SIZE)
+                                        color = (255,255,255)
+                                        color_letter = GREY
+                                        for item in colors:
+                                            if (i,j) == item[0]:
+                                                color = item[1]
+                                                color_letter = (255,255,255)
+                                        pygame.draw.rect(screen, color, letter_square, border_radius=3)
+                                        pygame.draw.rect(screen, GREY, letter_square, width=2)
+                                        letter = LETTERS_FONT_SMALL.render(ALPHABET[i][j], False, color_letter)
+                                        surface = letter.get_rect(center = (x+ (LETTERS_SIZE + 2*MARGIN) + LETTERS_SIZE//2, y+LETTERS_SIZE//2))
+                                        screen.blit(letter, surface)
+
+                                        x += LETTERS_SIZE + 2*MARGIN
+
+                                y += LETTERS_SIZE + 2*MARGIN
+                            # Show correct answer after game over
+                            if len(GUESSES) == 6 and GUESSES[5] != WORD_TO_GUESS:
+                                GAME_OVER = True
+                                letters = LETTERS_FONT.render(WORD_TO_GUESS, False, GREY)
+                                surface = letters.get_rect(center = (WINDOW_WIDTH//2.3, TOP_MARGIN//2 + 6 * (SQUARES_SIZE + MARGIN)  + TOP_MARGIN//2))
+                                screen.blit(letters, surface)
+                            
+                            if WIN:
+                                letters = LETTERS_FONT.render('IA WINS', False, GREY)
+                                surface = letters.get_rect(center = (WINDOW_WIDTH//2.3 - 10, TOP_MARGIN//2 + 6 * (SQUARES_SIZE + MARGIN)  + TOP_MARGIN//2))
+                                screen.blit(letters, surface)
+                            # Update screen
+                            pygame.display.flip()
+                            if WIN or DEFEAT:
+                                # Restart the game
+                                time.sleep(5)
+                                checkboxes = [False, False, False, False]
+                                GAME_OVER = False
+                                WORD_TO_GUESS = random.choice(ENGLISH_WORDS_5_LETTERS)
+                                GUESSES = []
+                                INPUT = ""
+                                break
+                            # Random wait time - To humanize the input of values
+                            random_time = random.uniform(0, 2)
+                            time.sleep(random_time)
 
             if pos[0] > WINDOW_WIDTH-260 and pos[0] < WINDOW_WIDTH-240:
                 if pos[1] > 187 and pos[1] < 207:
